b487bf6d83773ff8e447ec91aa61d9ace380b867
rosunshrestha <rosun.shrestha@gmail.com>
2015-06-20 01:17:04 +0545

    fire obstacle and collision

--- js/runner/state/play.js
+++ js/runner/state/play.js
@@ -14,17 +14,17 @@
             game.load.spritesheet('robot', 'assets/enemy2.png', 85.6, 128, 5);
             game.load.spritesheet('dog', 'assets/enemy3.png', 100, 58, 24);
             game.load.image('coin', 'assets/coin.png');
+            game.load.spritesheet('fire', 'assets/fire.png',55,55,16);
+
             //  Firefox doesn't support mp3 files, so use ogg
             game.load.audio('playing', ['assets/play.mp3', 'assets/play.ogg']);
             game.load.image('ground', 'assets/transplatform.png');
         },
         create: function () {
-
             this.game.renderer.renderSession.roundPixels = true;
             game.physics.startSystem(Phaser.Physics.ARCADE);
             this.physics.arcade.skipQuadTree = false;
             this.physics.arcade.gravity.y = 750;
-
             this.bg = game.add.tileSprite(0, 0, imageDim[0], imageDim[1], 'bg');
 
             // Level text display
@@ -52,12 +52,11 @@
 
             this.bg.scale.setTo(bgRatio, bgRatio);
             //this.hero.body.gravity.y = 1000;
-            var upArrow = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-            upArrow.onDown.add(this.jump, this);
+            var space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+            space.onDown.add(this.shoot, this);
 
 
             this.platforms = game.add.physicsGroup();
-
             this.ground = this.platforms.create(0, game.world.height - 50, 'ground');
             this.ground.width = game.world.width;
 
@@ -79,20 +78,23 @@
             this.hero.jump = function () {
                 this.body.velocity.y = -500;
             };
-
             this.cursors = this.input.keyboard.createCursorKeys();
-
             game.time.events.loop(Phaser.Timer.SECOND * 2, this.scoreUpdate, this);
-            this.makeEnemies();
-            this.makeCoins();
+            //this.makeCoins();
+            //this.makeEnemies();
         },
         makeCoins: function () {
-            this.coinGenerator = game.time.events.loop(Phaser.Timer.SECOND * 2 + 1, this.makeCoin, this);
+            this.coinGenerator = game.time.events.loop(Phaser.Timer.SECOND * game.rnd.integerInRange(3,8), this.makeCoin, this);
             this.coinGenerator.timer.start();
-            this.coin = game.add.group();
+            this.points = game.add.group();
         },
         makeCoin: function() {
-            game.add.tileSprite(0, 0, game.world.width/2, game.world.height/2, 'bg');
+            this.coin = game.add.sprite(1175, game.rnd.integerInRange(game.world.height/2 + 60, game.world.height - 80), 'coin');
+            this.coin.scale.setTo(0.2, 0.2);
+            game.physics.arcade.enable(this.coin);
+            this.points.add(this.coin);
+            this.coin.body.velocity.x = -200;
+            this.coin.body.allowGravity = false;
         },
         makeEnemies: function () {
             this.enemiesGenerator = game.time.events.loop(Phaser.Timer.SECOND * 2, this.makeEnemy, this);
@@ -110,7 +112,6 @@
             }
             else if(enemyType == 2) {
                 enemy = game.add.sprite(1175, 435, 'skeleton');
-
                 enemy.scale.setTo(0.3, 0.3);
                 enemy.animations.add('run', [0, 1, 2, 3], 4, true, true);
                 enemy.animations.play('run');
@@ -129,14 +130,20 @@
             enemy.body.velocity.x = -200;
             //this.enimies.setAll('body.velocity.x',-200);
         },
+
+        shoot: function () {
+            this.fire = game.add.sprite(300,420,'fire');
+            this.fire.animations.add('run');
+            this.fire.animations.play('run');
+            game.physics.arcade.enable(this.fire);
+            this.fire.body.velocity.x  = 300;
+            this.fire.allowGravity = false;
+        },
         jump: function () {
-            alert('hello');
             var jumps = game.add.tween(this.hero);
             jumps.to({y: this.hero.height - 60}, 6000, Phaser.Easing.Bounce.In);
             jumps.onComplete.add(jumps, this);
             jumps.start();
-
-
             //this.enimies.setAll('body.velocity.x',-200);
         },
         scoreUpdate: function () {
@@ -148,11 +155,13 @@
             this.currentScore.setText(runner.score.toString());
         },
         update: function () {
-
             // hold the hero and the enemies
             this.physics.arcade.collide(this.hero, this.platforms, null, null, this);
             this.physics.arcade.collide(this.enimies, this.platforms, null, null, this);
+           // this.physics.arcade.collide(this.enimies, this.hero, this.deathHandler, null, this);
 
+            this.physics.arcade.collide(this.coin, this.hero, this.removeCoin, null, this);
+            this.physics.arcade.collide(this.fire, this.enimies, this.removeEnemy, null, this);
             // only if the hero is on top of the group
             var onTheGround = (this.hero.bottom === this.ground.top);
 
@@ -169,7 +178,16 @@
             }
 
         },
+        removeEnemy: function(){
+            this.enimies.destroy();
+            this.fire.destroy();
+        },
+        removeCoin: function(){
+            this.coin.destroy();
+            runner.score += 20;
+            this.scoreUpdate();
 
+        },
         run: function (speed) {
             var magnitude = 500;
             var delta = 300;
